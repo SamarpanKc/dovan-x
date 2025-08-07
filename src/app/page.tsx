e11fa370b780428d5bf9953c6c71bd9b0fdf263b
SamarpanKc <samarpan0kc@gmail.com>
2025-08-07 15:47:27 +0545

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 text-5xl font-bold">
+      Hello, Samarpan KC
+      <button className="bg-primary text-black text-2xl px-3 py-2 rounded-2xl font-general-sans font-semibold" type="submit">Sign up</button>
+    </div>
+  );
+}
